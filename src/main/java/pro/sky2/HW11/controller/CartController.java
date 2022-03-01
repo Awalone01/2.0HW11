41d2464a459df92aa12b84c4810c750b235ade21
Awalone01 <sashaneiv01@yandex.ru>
2022-03-01 19:53:03 +0300

    Commit 2 HW11

--- src/main/java/pro/sky2/HW11/controller/CartController.java
+++ src/main/java/pro/sky2/HW11/controller/CartController.java
@@ -1,15 +1,14 @@
 package pro.sky2.HW11.controller;
 
-import org.springframework.stereotype.Controller;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import pro.sky2.HW11.service.CartService;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/order")
 public class CartController {
 
@@ -21,7 +20,7 @@ public class CartController {
 
 
     @GetMapping(path = "addItems")
-    public List<Integer> addItems(@RequestParam() List<Integer> idItem) {
+    public List<Integer> addItems(@RequestParam("idItem") List<Integer> idItem) {
         return cartService.addItems(idItem);
     }
 
